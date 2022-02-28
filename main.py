3319da24ded4b23146c4c537d6fed42abbba485d
sethusaim <sethusaim@gmail.com>
2022-02-28 13:46:24 +0530

    made new changes

---
+++ main.py
@@ -0,0 +1,47 @@
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import Response
+from fastapi.templating import Jinja2Templates
+
+from read_params import read_params
+
+app = FastAPI()
+
+config = read_params()
+
+templates = Jinja2Templates(directory=config["templates"]["dir"])
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def index(request: Request):
+    return templates.TemplateResponse(
+        config["templates"]["index"], {"request": request}
+    )
+
+
+@app.get("/test")
+async def trainRouteClient():
+    try:
+        return Response("Hello World")
+
+    except Exception as e:
+        return Response("Error Occurred! %s" % e)
+
+
+if __name__ == "__main__":
+    host = config["app"]["host"]
+
+    port = config["app"]["port"]
+
+    uvicorn.run(app, host=host, port=port)
